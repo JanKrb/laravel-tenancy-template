f4d9181d2b226f0988c8eee40ca03fb5e006bc8a
Jan Ruhfus <janruhfus0@gmail.com>
2022-06-06 17:43:32 +0200

    Load localization

---
+++ app/Http/Middleware/Localization.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\App;
+
+class Localization
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    {
+        $lang = $request->hasHeader('X-LANG') ? $request->header('X-LANG') : config('app.locale');
+
+        if (App::currentLocale() != $lang) {
+            App::setLocale($lang);
+        }
+
+        return $next($request);
+    }
+}
