27a3c44c281fe0ce9b3408f19aa5bc9be63c604d
Jan Ruhfus <janruhfus0@gmail.com>
2022-06-18 13:57:46 +0200

    Fix localization

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -13,11 +13,11 @@ class Localization
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return Response|RedirectResponse
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    public function handle(Request $request, Closure $next)
     {
         $lang = $request->hasHeader('X-LANG') ? $request->header('X-LANG') : config('app.locale');
 
